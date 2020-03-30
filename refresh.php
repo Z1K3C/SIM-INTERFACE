d48308cbd7a171be2109fce623585cdee5fad5ee
Z1K_3C <tripleec13@hotmail.com>
2020-03-30 11:40:56 -0600

    third modification

--- refresh.php
+++ refresh.php
@@ -1,15 +1,12 @@
 <?php
 $DSN = "SQL_1";
-$USER = "sa";
-$PASS = "otrogato";
-$LINK = odbc_connect($DSN,$USER,$PASS);
-$QUERY = odbc_exec($LINK, " SELECT * FROM 
-                            [AXIS].[dbo].[ROBOT_AXIS]");
+$USER = 'sa';
+$PASS= 'otrogato';
+$LINK=odbc_connect($DSN, $USER, $PASS);
+$QUERY_01 = odbc_exec($LINK,"SELECT * FROM [AXIS].[dbo].[ROBOT_AXIS]");
 $ROW = array();
-
-while($TABLE = odbc_fetch_object($QUERY)){
-    $ROW[] = $TABLE -> AXIS_N;
+while($TABLE_01 = odbc_fetch_object($QUERY_01)){
+	$ROW[]=$TABLE_01 -> AXIS_N;
 }
 echo json_encode($ROW);
-odbc_close($LINK);
 ?>
